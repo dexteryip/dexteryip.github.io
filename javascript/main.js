a5f8f5d6a821807432c998c6e6482cf07959f3b6
Dexter <dexteryip2003@yahoo.com.hk>
2022-04-06 15:36:13 +0800

    portfolio update

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+function r(f) { /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f() }
+// use like
+r(function() {
+    showSlides(1, 1);
+    showSlides(1, 2);
+    showSlides(1, 3);
+});
+let slideIndex = [1, 1, 1];
+
+// Next/previous controls
+function plusSlides(n, sender) {
+    showSlides(slideIndex[sender - 1] += n, sender);
+}
+
+// Thumbnail image controls
+function currentSlide(n, sender) {
+    showSlides(slideIndex[sender - 1] = n, sender);
+}
+
+function showSlides(n, sender) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides" + sender);
+    let dots = document.getElementsByClassName("dot" + sender);
+    if (n > slides.length) { slideIndex[sender - 1] = 1 }
+    if (n < 1) { slideIndex[sender - 1] = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex[sender - 1] - 1].style.display = "block";
+    dots[slideIndex[sender - 1] - 1].className += " active";
+}
